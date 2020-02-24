8e80ec0760e816124acba00e7660ab10fe7a5bcb
Chaomow <wayne415231@gmail.com>
2020-02-24 13:53:29 +0800

    202002241325

--- src/index.js
+++ src/index.js
@@ -26,7 +26,28 @@ const data = {
       "程式語言",
       "版本控制",
       "編輯工具"
-    ]
+    ],
+    workInfo: {
+      title: ["我的聊天室", "我的履歷", "即時通訊班務管理系統", "佐證資料"],
+      description: [
+        "該專案是自學React框架等前端應用的練習。</br>使用React JS框架，結合Ant Design、Node JS、NPM、Mongo DB，呈現一個簡易的聊天室。",
+        "該履歷使用HTML、CSS、JA履歷使用HTML、CSS、Javascript等前端語言，結合FontAwesome的圖示以及響應式網頁的設計概念所製作。",
+        "使用者透過MSN、Yahoo即時通與本系統進行班務活動。</br>" +
+          "透過讀取資料庫學生帳號資料，對平台MSN、Yahoo即時通噗浪(Plurk)、臉書(Facebook)、Gmail以及SMS簡訊發送即時通訊公告。</br>" +
+          "使用者亦可透過本系統將日常行事曆做紀錄，並透過簡訊功能提醒行程。</br>" +
+          "透過權限分級，使用者可讀取資料庫班費與租賃資料，總務股長可隨時方便進行班費資料的新增、修改、刪除。</br>" +
+          "同學可於本系統修改個人租賃資料，而租賃股長可調閱全班學生聯絡資訊。",
+        "大學專題 - 虛擬實境健身腳踏車系統</br>" +
+          "大學專題 - 班務管理系統</br>" +
+          "英文檢定 - 雅思均6分(對照多益750分)</br>" +
+          "學歷文憑 - Stanley College West Australia</br>" +
+          "學歷文憑 - 高雄應用科技大學資訊管理系</br>" +
+          "學歷文憑 - 大安高級工業職業學校綜合高中</br>" +
+          "比賽獎項 - 全國網頁競賽</br>" +
+          "工作表現 - Aloft Perth 年度最佳員工提名</br>" +
+          "工作表現 - Aloft Perth 季度最佳員工提名</br>"
+      ]
+    }
   },
   english: {
     menu: ["HOME", "ABOUT", "SKILL", "WORKS"],
@@ -56,9 +77,31 @@ const data = {
       "Coding Language",
       "version COntrol",
       "Code Editor"
-    ]
-  },
-  codeLogo: {}
+    ],
+    workInfo: {
+      title: [
+        "My Chatting Project",
+        "My resume",
+        "Class Management System",
+        "Certification"
+      ],
+      description: [
+        "This program is a simple chatting room with communication applications.</br></br>Use React JS as a framework, go with Ant Design、Node JS、NPM、Mongo DB etc. A practice for learning front-end skills.",
+        "A simple project to practice HTML, CSS, Javascript and responsive web design.",
+        "The program is made for managing class daily work, such as student information edit, revenue and expenditure of class events, function calendar and reminder.</br></br>" +
+          "Use MSN and Yahoo! Messenger as devices, communicate with Facebook, Plurk,  Gmail, Google calendar and Google Cloud database.",
+        "University Topics - Virtual reality fitness system</br>" +
+          "University Topics - Class Management System</br>" +
+          "Certification - Ielts average score 6 (compare to TOEIC score 750) </br>" +
+          "Certification - Stanley College Perth Western Australia, Cookery and Hospitality</br>" +
+          "Certification - National Kaohsiung University of Applied Sciences, Department of Information Management</br>" +
+          "Certification - Taipei Municipal Da-An Vocational High School, Departments of Comprehensive</br>" +
+          "Award - National Web design competition</br>" +
+          "Working performance - Aloft Perth Annualy ICONIC Awards</br>" +
+          "Working performance - Aloft Perth Seasonal ICONIC Awards</br>"
+      ]
+    }
+  }
 };
 
 function onScroll() {
@@ -97,6 +140,7 @@ function switchLanguage() {
     .getElementsByTagName("p")[1];
   const skillCat = document.getElementsByClassName("skillCatList");
   const skillArea = document.getElementsByClassName("skillArea");
+  const workInfo = document.getElementsByClassName("workInfo");
 
   if (language.getAttribute("value") === "chinese") {
     document.title = "Wayne Chao";
@@ -134,6 +178,13 @@ function switchLanguage() {
       skillArea[key].getElementsByTagName("p")[0].innerHTML =
         data.english.skillArea[key];
     }
+    //Work Infomation
+    for (var key = 0; key < workInfo.length; key++) {
+      workInfo[key].getElementsByTagName("p")[0].innerHTML =
+        data.english.workInfo.title[key];
+      workInfo[key].getElementsByTagName("p")[1].innerHTML =
+        data.english.workInfo.description[key];
+    }
   } else {
     document.title = "趙仲尉";
     language.setAttribute("value", "chinese");
@@ -169,6 +220,13 @@ function switchLanguage() {
     for (var key = 0; key < skillArea.length; key++) {
       skillArea[key].getElementsByTagName("p")[0].innerHTML =
         data.chinese.skillArea[key];
+    }
+    //Work Infomation
+    for (var key = 0; key < workInfo.length; key++) {
+      workInfo[key].getElementsByTagName("p")[0].innerHTML =
+        data.chinese.workInfo.title[key];
+      workInfo[key].getElementsByTagName("p")[1].innerHTML =
+        data.chinese.workInfo.description[key];
     }
   }
 }
